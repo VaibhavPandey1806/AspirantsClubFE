f575b9512af80ab4805387154ab372c39f29d44f
VaibhavPandey1806 <vaibhav.ppp78@gmail.com>
2024-11-13 17:12:03 +0530

    Added cloud url

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,4 @@
 // src/constants.js
 // export const BASE_URL = 'http://localhost:8081/api';
-export const BASE_URL = 'http://192.168.31.38:8081/api';
+// export const BASE_URL = 'http://192.168.31.38:8081/api';
+export const BASE_URL = 'aspirantsclub-production.up.railway.app:8081/api';
