fb62d2b55bc183db152870dfd7f004f1a962365a
Vaibhav <vaibhav.ppp78@gmail.com>
2024-11-28 22:15:57 +0530

    Add Questions new layout

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -7,7 +7,6 @@ import Footer from './Footer';
 
 axios.defaults.withCredentials = true;
 
-
 const AddQuestion = () => {
   const [questionText, setQuestionText] = useState('');
   const [options, setOptions] = useState({ A: '', B: '', C: '', D: '' });
@@ -123,135 +122,138 @@ const AddQuestion = () => {
   };
 
   return (
-    <><div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Header />
-      <div className="add-question-container" style={{ flex: 1 }}>
-        <h1 className="add-question-title">Add Question</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Question:</label>
-            <input
-              type="text"
-              value={questionText}
-              onChange={(e) => setQuestionText(e.target.value)}
-              required
-              className={renderInputClass('questionText')} />
-          </div>
-          <div className="form-group">
-            <label>Options:</label>
-            {['A', 'B', 'C', 'D'].map((option) => (
+    <>
+      <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <Header />
+        <div className="add-question-container" style={{ flex: 1 }}>
+          <h1 className="add-question-title">Add Question</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Question:</label>
               <input
-                key={option}
                 type="text"
-                value={options[option]}
-                onChange={(e) => handleOptionChange(option, e.target.value)}
+                value={questionText}
+                onChange={(e) => setQuestionText(e.target.value)}
                 required
-                placeholder={`Option ${option}`}
-                className={renderInputClass('options')} />
-            ))}
-          </div>
-          <div className="correct-answer-options">
-            <label>Correct Answer:</label>
-            {['A', 'B', 'C', 'D'].map((option) => (
-              <div key={option}>
+                className={renderInputClass('questionText')} />
+            </div>
+            <div className="form-group">
+              <label>Options:</label>
+              {['A', 'B', 'C', 'D'].map((option) => (
                 <input
-                  type="radio"
-                  name="correctAnswer"
-                  checked={correctAnswer === option}
-                  onChange={() => setCorrectAnswer(option)}
-                  required />
-                <label>{option}</label>
-              </div>
-            ))}
-          </div>
-          <div className="form-group">
-            <label>Category Type:</label>
-            <input
-              type="radio"
-              value="existing"
-              checked={selectedCategoryType === 'existing'}
-              onChange={() => setSelectedCategoryType('existing')} /> Existing
-            <input
-              type="radio"
-              value="new"
-              checked={selectedCategoryType === 'new'}
-              onChange={() => setSelectedCategoryType('new')} /> New
-          </div>
-          {selectedCategoryType === 'existing' ? (
-            <select value={category} onChange={(e) => setCategory(e.target.value)} className={renderInputClass('category')}>
-              <option value="">Select Category</option>
-              {categories.map((cat, index) => (
-                <option key={index} value={cat}>{cat}</option>
+                  key={option}
+                  type="text"
+                  value={options[option]}
+                  onChange={(e) => handleOptionChange(option, e.target.value)}
+                  required
+                  placeholder={`Option ${option}`}
+                  className={renderInputClass('options')} />
               ))}
-            </select>
-          ) : (
-            <input
-              type="text"
-              value={newCategory}
-              onChange={(e) => setNewCategory(e.target.value)}
-              required
-              className={renderInputClass('newCategory')} />
-          )}
-          <div className="form-group">
-            <label>Topic Type:</label>
-            <input
-              type="radio"
-              value="existing"
-              checked={selectedTopicType === 'existing'}
-              onChange={() => setSelectedTopicType('existing')} /> Existing
-            <input
-              type="radio"
-              value="new"
-              checked={selectedTopicType === 'new'}
-              onChange={() => setSelectedTopicType('new')} /> New
-          </div>
-          {selectedTopicType === 'existing' ? (
-            <select value={topic} onChange={(e) => setTopic(e.target.value)} className={renderInputClass('topic')}>
-              <option value="">Select Topic</option>
-              {topics.map((top, index) => (
-                <option key={index} value={top}>{top}</option>
+            </div>
+            <div className="correct-answer-options">
+              <span style={{ marginRight: '10px', fontWeight: 'bold' }}>Correct Answer:</span>
+              {['A', 'B', 'C', 'D'].map((option) => (
+                <div key={option}>
+                  <input
+                    type="radio"
+                    name="correctAnswer"
+                    checked={correctAnswer === option}
+                    onChange={() => setCorrectAnswer(option)}
+                    required />
+                  <label>{option}</label>
+                </div>
               ))}
-            </select>
-          ) : (
-            <input
-              type="text"
-              value={newTopic}
-              onChange={(e) => setNewTopic(e.target.value)}
-              required
-              className={renderInputClass('newTopic')} />
-          )}
-          <div className="form-group">
-            <label>Source Type:</label>
-            <input
-              type="radio"
-              value="existing"
-              checked={selectedSourceType === 'existing'}
-              onChange={() => setSelectedSourceType('existing')} /> Existing
-            <input
-              type="radio"
-              value="new"
-              checked={selectedSourceType === 'new'}
-              onChange={() => setSelectedSourceType('new')} /> New
-          </div>
-          {selectedSourceType === 'existing' ? (
-            <select value={source} onChange={(e) => setSource(e.target.value)} className={renderInputClass('source')}>
-              <option value="">Select Source</option>
-              {sources.map((src, index) => (
-                <option key={index} value={src}>{src}</option>
-              ))}
-            </select>
-          ) : (
-            <input
-              type="text"
-              value={newSource}
-              onChange={(e) => setNewSource(e.target.value)}
-              required
-              className={renderInputClass('newSource')} />
-          )}
-          <button type="submit">Submit</button>
-        </form>
+            </div>
+            <div className="form-group">
+              <label>Category Type:</label>
+              <input
+                type="radio"
+                value="existing"
+                checked={selectedCategoryType === 'existing'}
+                onChange={() => setSelectedCategoryType('existing')} /> Existing
+              <input
+                type="radio"
+                value="new"
+                checked={selectedCategoryType === 'new'}
+                onChange={() => setSelectedCategoryType('new')} /> New
+            </div>
+            {selectedCategoryType === 'existing' ? (
+              <select value={category} onChange={(e) => setCategory(e.target.value)} className={renderInputClass('category')}>
+                <option value="">Select Category</option>
+                {categories.map((cat, index) => (
+                  <option key={index} value={cat}>{cat}</option>
+                ))}
+              </select>
+            ) : (
+              <input
+                type="text"
+                value={newCategory}
+                onChange={(e) => setNewCategory(e.target.value)}
+                required
+                className={renderInputClass('newCategory')} />
+            )}
+            <div className="form-group">
+              <label>Topic Type:</label>
+              <input
+                type="radio"
+                value="existing"
+                checked={selectedTopicType === 'existing'}
+                onChange={() => setSelectedTopicType('existing')} /> Existing
+              <input
+                type="radio"
+                value="new"
+                checked={selectedTopicType === 'new'}
+                onChange={() => setSelectedTopicType('new')} /> New
+            </div>
+            {selectedTopicType === 'existing' ? (
+              <select value={topic} onChange={(e) => setTopic(e.target.value)} className={renderInputClass('topic')}>
+                <option value="">Select Topic</option>
+                {topics.map((top, index) => (
+                  <option key={index} value={top}>{top}</option>
+                ))}
+              </select>
+            ) : (
+              <input
+                type="text"
+                value={newTopic}
+                onChange={(e) => setNewTopic(e.target.value)}
+                required
+                className={renderInputClass('newTopic')} />
+            )}
+            <div className="form-group">
+              <label>Source Type:</label>
+              <input
+                type="radio"
+                value="existing"
+                checked={selectedSourceType === 'existing'}
+                onChange={() => setSelectedSourceType('existing')} /> Existing
+              <input
+                type="radio"
+                value="new"
+                checked={selectedSourceType === 'new'}
+                onChange={() => setSelectedSourceType('new')} /> New
+            </div>
+            {selectedSourceType === 'existing' ? (
+              <select value={source} onChange={(e) => setSource(e.target.value)} className={renderInputClass('source')}>
+                <option value="">Select Source</option>
+                {sources.map((src, index) => (
+                  <option key={index} value={src}>{src}</option>
+                ))}
+              </select>
+            ) : (
+              <input
+                type="text"
+                value={newSource}
+                onChange={(e) => setNewSource(e.target.value)}
+                required
+                className={renderInputClass('newSource')} />
+            )}
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
-    </div><div><Footer></Footer></div></>
+      <div><Footer /></div>
+    </>
   );
 };
 
